805fbd02dfb822ab2526d0f053bc662a8a997700
Douglas Guimarães <douglas.guimaraes.2120@gmail.com>
2025-02-22 12:19:44 -0300

    fix: redirect to lang and add: article logic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,20 @@
-import { NextIntlClientProvider } from "next-intl";
-import { redirect } from "next/navigation";
-import { routing } from "@/i18n/routing";
-
 import type { Metadata } from "next";
 import { Pixelify_Sans } from "next/font/google";
 
-import { locales } from "@/i18n/routing";
-import { setRequestLocale } from "next-intl/server";
+import { redirect } from "next/navigation";
 
 const font = Pixelify_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Finituz Blog",
-  description: "The official Finituz game studio blog website.",
+  title: "Finituz Game Studio",
+  description: "The official Finituz game studio website.",
 };
 
-export async function generateStaticParams() {
-  return locales.map((locale) => ({ locale }));
-}
-
-export default async function LocaleLayout({
-  children,
-  params: { locale },
-}: {
-  children: React.ReactNode;
-  params: { locale: string };
-}) {
-  // Ensure that the incoming `locale` is valid
-
-  setRequestLocale(locale);
-
-  // Providing all messages to the client
-  // side is the easiest way to get started
-
+export default async function LocaleLayout() {
   return (
-    <html lang={locale}>
+    <html>
       <body className={font.className + " text-xl text-white bg-black"}>
-        {redirect(`/${locale}`)}
+        {redirect("/en")}
       </body>
     </html>
   );
