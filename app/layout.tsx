b6f99fb38a8bddd33ae055a7dea205b0d7b77719
Douglas Guimarães <douglas.guimaraes.2120@gmail.com>
2025-02-22 12:46:33 -0300

    fix: redirect to lang and add: article logic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Pixelify_Sans } from "next/font/google";
 
-import { redirect } from "next/navigation";
-
 const font = Pixelify_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -10,11 +8,11 @@ export const metadata: Metadata = {
   description: "The official Finituz game studio website.",
 };
 
-export default async function LocaleLayout() {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html>
       <body className={font.className + " text-xl text-white bg-black"}>
-        {redirect("/en")}
+        {children}
       </body>
     </html>
   );
