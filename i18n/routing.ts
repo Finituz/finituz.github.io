1536c1efacfea2af892db0c7c376203427f2e79f
Douglas Guimarães <douglas.guimaraes.2120@gmail.com>
2025-02-22 15:50:22 -0300

    fix: redirect

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -6,8 +6,7 @@ export const locales = ["br", "en"];
 export const routing = defineRouting({
   // A list of all locales that are supported
   locales: locales,
-  localPrefix: "as-needed",
-
+  localePrefix: "always",
   // Used when no locale matches
   defaultLocale: locales[1],
 });
