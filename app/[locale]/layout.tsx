85b584e75a0fdf4d6f962e7edcbad74cdba811ad
Douglas Guimarães <douglas.guimaraes.2120@gmail.com>
2025-02-23 18:58:42 -0300

    fix: footer non-clickable

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -59,7 +59,11 @@ export default async function LocaleLayout({
         <meta name="google-adsense-account" content="ca-pub-9376449764182229" />
         <GoogleAdsense pId="9376449764182229" />
       </head>
-      <body className={font.className + "  text-xl bg-black text-white"}>
+      <body
+        className={
+          font.className + "  relative pb-64 text-xl bg-black text-white"
+        }
+      >
         <ParticlesBG />
         <NextIntlClientProvider locale={locale} messages={messages}>
           <Header />
