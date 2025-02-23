913a4fcbdd2624a66d085d51b2da3a52d8da3f62
Douglas Guimarães <douglas.guimaraes.2120@gmail.com>
2025-02-23 18:36:14 -0300

    feat: visual symbol article island

--- app/[locale]/components/blog/ArticleIsland/ArticleIsland.tsx
+++ app/[locale]/components/blog/ArticleIsland/ArticleIsland.tsx
@@ -5,17 +5,33 @@ import {
   RiThumbUpLine,
 } from "react-icons/ri";
 import Notification from "../../Notification/Notification";
-import { MouseEvent, useState } from "react";
+import { MouseEvent, useRef, useState } from "react";
 
 export default function ArticleIsland() {
   const [notification, setNotification] = useState({
     title: "",
     message: "",
   });
+  const ToggleSymbol = useRef<HTMLDivElement>(null);
 
   const ToggleIsland = (event: MouseEvent) => {
     event.stopPropagation();
-    (event.currentTarget as HTMLDivElement).classList.toggle("-left-16");
+    if (!ToggleSymbol.current) return;
+
+    if (event.currentTarget.classList.contains("-left-16")) {
+      (event.currentTarget as HTMLDivElement).classList.replace(
+        "-left-16",
+        "left-2",
+      );
+      ToggleSymbol.current.innerHTML = "<";
+    } else {
+      (event.currentTarget as HTMLDivElement).classList.replace(
+        "left-2",
+        "-left-16",
+      );
+
+      ToggleSymbol.current.innerHTML = ">";
+    }
   };
 
   const copyToShare = () => {
@@ -31,23 +47,31 @@ export default function ArticleIsland() {
     <>
       <div
         onClick={ToggleIsland}
-        className="fixed flex flex-col bg-red-900  transition-all duration-500 left-2 -left-16 top-1/2 transform -translate-y-1/2 gap-5 border border-white rounded-lg p-5 "
+        className="flex fixed w-fit bg-red-900  transition-all duration-500 left-2 top-1/2 transform -translate-y-1/2 border border-white rounded-lg"
       >
-        <span className="cursor-pointer hover:bg-red-400 transition-colors duration-500 rounded-xl p-2">
-          <RiThumbUpLine />
-        </span>
-        <span className="cursor-pointer hover:bg-red-400 transition-colors duration-500 rounded-xl p-2">
-          <RiThumbDownLine />
-        </span>
-        <span className="cursor-pointer hover:bg-red-400 transition-colors duration-500 rounded-xl p-2">
-          <RiMessageLine />
-        </span>
-        <span
-          onClick={copyToShare}
-          className="cursor-pointer hover:bg-red-400 transition-colors duration-500 rounded-xl p-2"
+        <div className="flex flex-col gap-5 p-5 pr-2">
+          <span className="cursor-pointer hover:bg-red-400 transition-colors duration-500 rounded-xl p-2">
+            <RiThumbUpLine />
+          </span>
+          <span className="cursor-pointer hover:bg-red-400 transition-colors duration-500 rounded-xl p-2">
+            <RiThumbDownLine />
+          </span>
+          <span className="cursor-pointer hover:bg-red-400 transition-colors duration-500 rounded-xl p-2">
+            <RiMessageLine />
+          </span>
+          <span
+            onClick={copyToShare}
+            className="cursor-pointer hover:bg-red-400 transition-colors duration-500 rounded-xl p-2"
+          >
+            <RiShareLine />
+          </span>
+        </div>
+        <div
+          ref={ToggleSymbol}
+          className="bg-red-400 rounded-tr-lg rounded-br-lg flex items-center p-1 cursor-pointer"
         >
-          <RiShareLine />
-        </span>
+          {"<"}
+        </div>
       </div>
       {notification.message.length > 0 ? (
         <Notification {...notification} />
