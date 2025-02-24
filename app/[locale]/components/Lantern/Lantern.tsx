8df7bc99650c89822291a4e94c884ca35e022bb4
Douglas Guimarães <douglas.guimaraes.2120@gmail.com>
2025-02-24 11:24:19 -0300

    fix latern on android

--- app/[locale]/components/Lantern/Lantern.tsx
+++ app/[locale]/components/Lantern/Lantern.tsx
@@ -2,7 +2,7 @@ export default function Lantern({ className }: { className?: string }) {
   return (
     <div
       className={
-        "absolute transform transparent shadow-[10px_80px_1000px_120px_#ff0000] " +
+        "absolute transform transparent shadow-[20px_80px_2000px_100px_#ff0000] " +
         className
       }
     ></div>
