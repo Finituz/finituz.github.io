928b3d88702f27730bdfde2c269625cf21378480
Douglas Guimarães <douglas.guimaraes.2120@gmail.com>
2025-02-28 08:53:20 -0300

    remove lantern on small devices

--- app/[locale]/components/blog/Manuals/Manuals.tsx
+++ app/[locale]/components/blog/Manuals/Manuals.tsx
@@ -14,28 +14,20 @@ export default function News({
   const createArticle = articlesData
     .filter((data) => data.tags.includes("manual"))
     .map((data, key) => {
-      const currentDate = new Date();
-      const createdAt = new Date(data.createdAt);
-      const timeDiff =
-        currentDate.getMonth() == createdAt.getMonth() &&
-        currentDate.getDate() - createdAt.getDate() < 7;
-
-      if (timeDiff) {
-        return (
-          <Link
-            href={`blog/article?title=${data.title[currentLanguage as keyof typeof data.title]}&path=${data.path}-${currentLanguage}&thumbnail=${data.thumbnail}`}
+      return (
+        <Link
+          href={`blog/article?title=${data.title[currentLanguage as keyof typeof data.title]}&path=${data.path}-${currentLanguage}&thumbnail=${data.thumbnail}`}
+          key={key}
+        >
+          <Card
             key={key}
-          >
-            <Card
-              key={key}
-              title={data.title[currentLanguage as keyof typeof data.title]}
-              imagePath={data.thumbnail}
-              imageAlt={data.thumbnailAlt}
-              isReleased
-            />
-          </Link>
-        );
-      }
+            title={data.title[currentLanguage as keyof typeof data.title]}
+            imagePath={data.thumbnail}
+            imageAlt={data.thumbnailAlt}
+            isReleased
+          />
+        </Link>
+      );
     });
   return (
     <div className="flex gap-10 w-full p-14 overflow-hidden">
