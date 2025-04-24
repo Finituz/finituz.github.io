326dd3f1a547655c97260b892abcd3be08cfdcf0
Douglas Guimarães <douglas.guimaraes.2120@gmail.com>
2025-04-24 09:45:36 -0300

    fix language path with multiple /

--- app/[locale]/components/ChangeLanguage/ChangeLanguage.tsx
+++ app/[locale]/components/ChangeLanguage/ChangeLanguage.tsx
@@ -36,7 +36,7 @@ function ChangeLanguage() {
               className={"flex hover:bg-red-500 p-2 rounded-xl " + selected}
             >
               <Link
-                href={`/${loc}/${currentPath}?${currentQuery}`}
+                href={`/${loc.replace("/", "")}/${currentPath}?${currentQuery}`}
                 className="flex"
               >
                 <Image
